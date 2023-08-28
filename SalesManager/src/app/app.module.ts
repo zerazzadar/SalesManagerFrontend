0c199370a49ceabcb6853d562dd46485a10b9cb1
Rafael <rafael.valdes@innovant.us>
2023-08-28 08:46:27 -0400

    FirstCommit

---
+++ SalesManager/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AngularMaterialModule } from "./angular-material.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
